6300106830860b2381ab89e6ad1768ab0f6d6fd5
GBKUSER <zixona@gmail.com>
2026-02-20 02:18:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ app.get("/", (req, res) => res.send("OK: server is up"));
 app.get("/api/health", (req, res) => res.json({ ok: true }));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Listening on", PORT);
-});
+app.listen(PORT, "0.0.0.0", () => console.log("Listening on", PORT));
